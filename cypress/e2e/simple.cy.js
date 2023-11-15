240310c4f46d7c082809ac1175c46e1dd041e10a
Jakub <jakubfedyk@gmail.com>
2023-11-15 22:16:02 +0100

    back to action

---
+++ cypress/e2e/simple.cy.js
@@ -0,0 +1,17 @@
+describe('My first simple test with Cypress', () => {
+	it('True should be True', () => {
+		expect(true).to.equal(true)
+	})
+
+	it('5 should be 5'),
+		() => {
+			expect(5).to.equal(5)
+		}
+})
+
+
+describe('Another describe block', () => {
+    it('False will be false', () => {
+        expect(false).to.equal(false)
+    })
+})
